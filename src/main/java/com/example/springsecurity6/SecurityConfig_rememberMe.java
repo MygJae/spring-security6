a23a8d397852424dc21472d9fb1d303e2c3f61f5
Mygjae <50861514+MygJae@users.noreply.github.com>
2024-08-25 23:20:43 +0900

    기억하기 인증 - rememberme()

--- src/main/java/com/example/springsecurity6/SecurityConfig_rememberMe.java
+++ src/main/java/com/example/springsecurity6/SecurityConfig_rememberMe.java
@@ -1,35 +1,34 @@
 package com.example.springsecurity6;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 
-import java.io.IOException;
-
 import static org.springframework.security.config.Customizer.withDefaults;
-/*
+
 @EnableWebSecurity
 @Configuration
-public class SecurityConfig_httpBasic {
+public class SecurityConfig_rememberMe {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
             .authorizeHttpRequests(auth -> auth
                 .anyRequest().authenticated())
-            .httpBasic(basic -> basic.authenticationEntryPoint(new CustomAuthenticationEntryPoint() {
-            }) );
+                .formLogin(withDefaults())
+                .rememberMe(rememberMe -> rememberMe
+//                        .alwaysRemember(true)
+                        .tokenValiditySeconds(3600)
+                        .userDetailsService(userDetailsService())
+                        .rememberMeParameter("remember-me")
+                        .rememberMeCookieName("remember-me-cookie")
+                        .key("remember-me-sample"));
         return http.build();
     }
 
@@ -45,4 +44,3 @@ public class SecurityConfig_httpBasic {
 
 }
 
-*/
